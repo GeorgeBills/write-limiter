c0178f9e85abf794720cf723f3d0fc02f25d881e
George Bills <george.s.bills@gmail.com>
2019-01-13 13:29:16 +1100

    Write out last value every few seconds from a random source.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"math/rand"
 	"sync"
 	"time"
 )
@@ -10,21 +11,40 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(1)
 	toWrite := make(chan int)
+	timer := time.Tick(1 * time.Second)
 	go generate(toWrite)
-	go write(&wg, toWrite)
+	go write(&wg, toWrite, timer)
 	wg.Wait()
 }
 
 func generate(out chan int) {
+	source := rand.NewSource(time.Now().UnixNano())
+	rnd := rand.New(source)
 	for i := 0; i <= 10; i++ {
 		out <- i
-		time.Sleep(1 * time.Second)
+		time.Sleep(time.Duration(rnd.Intn(3000)) * time.Millisecond)
 	}
+	close(out)
 }
 
-func write(wg *sync.WaitGroup, in chan int) {
-	for i := range in {
-		log.Printf("Writing %d", i)
+func write(wg *sync.WaitGroup, in chan int, ticker <-chan time.Time) {
+	var next int
+LOOP:
+	for {
+		select {
+		case i, ok := <-in:
+			if !ok {
+				break LOOP
+			}
+			next = i
+		case <-ticker:
+			if next > 0 {
+				log.Printf("Writing %d", next)
+			} else {
+				log.Printf("Nothing to write")
+			}
+			next = -1
+		}
 	}
 	wg.Done()
 }
